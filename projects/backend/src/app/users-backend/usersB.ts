cb2a7bd2d5748c7b424da90247cb6a5e0bf1b8f0
Arnaud LINDER <arnaud.linder@lacooperativewelcoop.com>
2019-06-19 14:54:49 +0200

    relation user

--- projects/backend/src/app/users-backend/usersB.ts
+++ projects/backend/src/app/users-backend/usersB.ts
@@ -1,11 +1,14 @@
 export class UsersB {
-    'id': number;
-    'email': string;
-    'roles': any;
-    'password': string;
-    'birthday': string;
-    'creation': string;
-    'avatar': string;
-    'lastName': string;
-    'firstName': string;
+    id: number;
+    email: string;
+    roles: any;
+    password: string;
+    birthday: string;
+    creation: string;
+    avatar: string;
+    lastName: string;
+    firstName: string;
+    premiumId: [
+        string
+    ];
 }
